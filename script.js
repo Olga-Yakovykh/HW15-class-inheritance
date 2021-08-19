e773c7c4c1fabf2897205230593287bd339c6b58
olga <mega.denisenko@ukr.net>
2021-08-19 13:57:42 +0300

    fix

--- script.js
+++ script.js
@@ -1,59 +1,11 @@
 let div1 = document.querySelector('#time');
 let div2 = document.querySelector('#clock');
 let div3 = document.querySelector('#timer');
-
 let Time = function(element) {
   this.element = element; 
-  this.element.addEventListener('click',() => {this.toggle()}) 
 }
-  
+ 
 Time.prototype.render = function() {
-  let now = new Date();
-  let h = now.getHours().toString();
-  let m = now.getMinutes().toString();
-  let s = now.getSeconds().toString()
-    if (h < 10) {
-      h = '0' + h;
-    }
-    if (m < 10) {
-      m = '0' + m;
-    }
-    if (s < 10) {
-      s = '0' + s;
-    }
-    if (this.element.classList.contains('hidden')) {
-      this.element.innerHTML = h + ':' + m + ':' + s;
-    } else {
-      this.element.innerHTML = h + ':' + m;
-    } 
-}
-
-Time.prototype.getting = function() {
-     setInterval(() => {this.render()
-    }, 100);
-}
-
-Time.prototype.toggle = function() {
-    this.element.classList.toggle('hidden'); 
-      }; 
-
-function Clock1 (element) {
-  Time.call(this, element)
-}
-
-Clock1.prototype = Object.create(Time.prototype);
-let shotClock = new Time(div1);
-shotClock.getting();
-shotClock.toggle();
-
-function Clock2(element) {
-  Clock1.call(this, element);
-}
-
-Clock2.prototype = Object.create(Clock1.prototype);
-let longClock = new Time(div2);
-
-longClock.render = function() {
   let now = new Date();
   let h = now.getHours().toString();
   let m = now.getMinutes().toString();
@@ -67,17 +19,34 @@ longClock.render = function() {
     if (s < 10) {
       s = '0' + s;
     }
-    if (this.element.classList.contains('hidden')) {
+    if (this.isFormat) {
       this.element.innerHTML = h + ':' + m;
     } else {
       this.element.innerHTML = h + ':' + m + ':' + s;
     } 
 }
+Time.prototype.toggle = function() {
+  this.isFormat = !this.isFormat;
+}
+let time = new Time(div1);
 
-longClock.getting();
-longClock.toggle();
+function ShotClock(element) {
+  Time.call(this, element);
+}
+ShotClock.prototype = Object.create(Time.prototype);
+let shotClock = new ShotClock(div2);
+div2.addEventListener('click',() => {shotClock.toggle()});
 
-console.log(Time);
-console.log(shotClock);
-console.log(longClock);
+function LongClock(element) {
+  Time.call(this, element);
+}
+LongClock.prototype = Object.create(Time.prototype);
+let longClock = new LongClock(div3);
+div3.addEventListener('click',() => {longClock.toggle()});
 
+setInterval(() => {
+  longClock.render();
+  shotClock.render();
+}, 100);
+
+console.log(time, shotClock, longClock);
